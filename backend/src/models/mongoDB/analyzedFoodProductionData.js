d853e837110e5f900ee981ca173858ad4f06d51f
Shubhangi Yadav <shubhangi.yadav@sjsu.edu>
2020-11-01 14:20:01 -0800

    Added analyzed food production database model schema

---
+++ backend/src/models/mongoDB/analyzedFoodProductionData.js
@@ -0,0 +1,44 @@
+`use strict`
+
+import mongoose from 'mongoose'
+
+const AnalyzedFoodProductionData = new mongoose.Schema({
+    year: {
+		type: String,
+        required: true
+    },
+    month: {
+		type: String,
+        required: true
+    },
+    state: {
+		type: String,
+        required: true
+    },
+    category: {
+		type: String,
+        required: true
+    },
+    commodity: {
+		type: String,
+        required: true
+    },
+    dataItem: {
+		type: String,
+        required: true
+    },
+    unit: {
+		type: String,
+        required: true
+    },
+    value: {
+		type: String,
+        required: true
+    },
+    disasterType: [{
+		type: String,
+        required: true
+    }],
+}, { versionKey: false })
+
+export default mongoose.model('analyzedFoodProductionData', AnalyzedFoodProductionData)
