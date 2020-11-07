3291b7ae95f46a501305c963a53891aaf8ca1725
Shubhangi Yadav <shubhangi.yadav@sjsu.edu>
2020-11-07 01:58:43 -0800

    Fixed bulk insertion script

--- backend/src/scripts/bulkInsertionFromCsvToDB.js
+++ backend/src/scripts/bulkInsertionFromCsvToDB.js
@@ -2,13 +2,20 @@ const fs = require("fs");
 const mongodb = require("mongodb").MongoClient;
 const fastcsv = require("fast-csv");
 
-// let url = "mongodb://username:password@localhost:27017/";
+
+let arguments = process.argv.slice(2);
+console.log(arguments.length, arguments[0], arguments[1])
 let url = "mongodb+srv://admin:cmpe295dbpassword@development-database.ifuhy.mongodb.net/";
-let stream = fs.createReadStream("datasets/processed_data.csv");
+let stream = fs.createReadStream(`datasets/${arguments[0]}`);
 let csvData = [];
 let csvStream = fastcsv
   .parse()
   .on("data", function(data) {
+    let disasterTypes = data[8].split(",")
+    let disasterTypeArr = [];
+    if(disasterTypes.length >= 1 && disasterTypes[0].length >= 1) {
+        disasterTypeArr = disasterTypes
+    }
     csvData.push({
       year: data[0],
       month: data[1],
@@ -18,7 +25,7 @@ let csvStream = fastcsv
       dataItem: data[5],
       unit: data[6],
       value: data[7],
-      disasterType: data[8]
+      disasterType: disasterTypeArr
     });
   })
   .on("end", function() {
@@ -35,7 +42,7 @@ let csvStream = fastcsv
 
         client
           .db("cmpe295")
-          .collection("analyzedfoodproductiondatas")
+          .collection("analyzedfoodproductiondatas") // change database name according to your need, use mongodb database name
           .insertMany(csvData, (err, res) => {
             if (err) throw err;
 
