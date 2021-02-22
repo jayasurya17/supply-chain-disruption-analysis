f59383e8b33ce650808b56974ee66625855f3225
Jayasurya17 <jayasurya1796@gmail.com>
2021-02-22 11:55:07 -0800

    renamed columns

--- backend/src/python/drugDataset.py
+++ backend/src/python/drugDataset.py
@@ -42,6 +42,86 @@ def add_percent_change(df):
 	df = df.apply(lambda x: find_percent_change(df, x), axis = 1)
 	return df
 
+def replace_states(df):
+	state_code = {
+	    "AL": "Alabama",
+	    "AK": "Alaska",
+	    "AS": "American Samoa",
+	    "AZ": "Arizona",
+	    "AR": "Arkansas",
+	    "CA": "California",
+	    "CO": "Colorado",
+	    "CT": "Connecticut",
+	    "DE": "Delaware",
+	    "DC": "District Of Columbia",
+	    "FM": "Federated States Of Micronesia",
+	    "FL": "Florida",
+	    "GA": "Georgia",
+	    "GU": "Guam",
+	    "HI": "Hawaii",
+	    "ID": "Idaho",
+	    "IL": "Illinois",
+	    "IN": "Indiana",
+	    "IA": "Iowa",
+	    "KS": "Kansas",
+	    "KY": "Kentucky",
+	    "LA": "Louisiana",
+	    "ME": "Maine",
+	    "MH": "Marshall Islands",
+	    "MD": "Maryland",
+	    "MA": "Massachusetts",
+	    "MI": "Michigan",
+	    "MN": "Minnesota",
+	    "MS": "Mississippi",
+	    "MO": "Missouri",
+	    "MT": "Montana",
+	    "NE": "Nebraska",
+	    "NV": "Nevada",
+	    "NH": "New Hampshire",
+	    "NJ": "New Jersey",
+	    "NM": "New Mexico",
+	    "NY": "New York",
+	    "NC": "North Carolina",
+	    "ND": "North Dakota",
+	    "MP": "Northern Mariana Islands",
+	    "OH": "Ohio",
+	    "OK": "Oklahoma",
+	    "OR": "Oregon",
+	    "PW": "Palau",
+	    "PA": "Pennsylvania",
+	    "PR": "Puerto Rico",
+	    "RI": "Rhode Island",
+	    "SC": "South Carolina",
+	    "SD": "South Dakota",
+	    "TN": "Tennessee",
+	    "TX": "Texas",
+	    "UT": "Utah",
+	    "VT": "Vermont",
+	    "VI": "Virgin Islands",
+	    "VA": "Virginia",
+	    "WA": "Washington",
+	    "WV": "West Virginia",
+	    "WI": "Wisconsin",
+	    "WY": "Wyoming"
+	}
+	df['state'] = df['state'].replace(state_code)
+	return df
+
+def replace_column_names(df):
+	new_names = {
+		"State": "state",
+		"Year": "year",	
+		"Quarter": "quarter",
+		"Product Name": "commodity",
+		"Units Reimbursed": "value",
+		"percent": "percent"
+
+	}
+	df = df.rename(new_names, axis='columns')
+	column_names = ['year', 'quarter', 'state', 'commodity', 'value', 'percent']
+	df = df.reindex(columns=column_names)
+	return df
+
 if __name__ == "__main__":
 	
 	directory = "datasets/state_drug_utilization/"
@@ -60,5 +140,15 @@ if __name__ == "__main__":
 
 	print ("Calculating percentage change")
 	allData = add_percent_change(allData)
+	allData = replace_column_names(allData)
+	allData = replace_states(allData)
 	print (allData)
 	save_to_csv(allData, "datasets/medicine_quarterly.csv")
+
+# FLUOXETINE: It can treat depression, obsessive-compulsive disorder (OCD), bulimia nervosa, and panic disorder.
+# CHLORHEXID: Topical antiseptic
+# PREDNISONE: It can treat many diseases and conditions, especially those associated with inflammation
+# DEXAMETHAS: It can treat inflammation 
+# PROMETHAZI: It can treat allergies and motion sickness. This medication can also help control pain, nausea, and vomiting. 
+# WARFARIN S: Helps to reduce the risk of a stroke or heart attack 
+# HEPARIN SO: Specifically it is also used in the treatment of heart attacks and unstable angina.
