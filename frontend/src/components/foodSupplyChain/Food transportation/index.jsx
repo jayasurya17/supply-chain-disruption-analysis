c4390a902cc1e26fc886f35b38088677e0d167c6
Aswin Prasad <aswinpchn@gmail.com>
2021-02-28 18:57:15 -0800

    Frontend: Added State Export details tab

--- frontend/src/components/foodSupplyChain/Food transportation/index.jsx
+++ frontend/src/components/foodSupplyChain/Food transportation/index.jsx
@@ -4,6 +4,7 @@ import { Tabs } from 'antd'
 import StatewiseImportDependacy from './Statewise import dependency'
 import StatewiseImportExportComparision from './Statewise import export comparision'
 import FoodExportHeatMap from './Food Export Heat Map'
+import StatewiseExportDetails from './Statewise export details'
 
 const { TabPane } = Tabs
 
@@ -19,10 +20,13 @@ const FoodTransportationHome = () => {
         <TabPane tab='Statewise import dependancy' key='1'>
           <StatewiseImportDependacy />
         </TabPane>
-        <TabPane tab='Statewise import export comparision' key='2'>
+        <TabPane tab='Statewise export details' key='2'>
+          <StatewiseExportDetails />
+        </TabPane>
+        <TabPane tab='Statewise import export comparision' key='3'>
           <StatewiseImportExportComparision />
         </TabPane>
-        <TabPane tab='Export by states heat map' key='3'>
+        <TabPane tab='Export by states heat map' key='4'>
           <FoodExportHeatMap />
         </TabPane>
       </Tabs>
